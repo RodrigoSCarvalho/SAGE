8f7aa9b94490295ee5654f283ccf6a4f189953bb
RodrigoSCarvalho <rodrigo26carvalho@gmail.com>
2025-08-27 13:51:17 -0300

    Altera connectionString

--- SAGE.Entrypoint/Program.cs
+++ SAGE.Entrypoint/Program.cs
@@ -14,7 +14,9 @@ var builder = WebApplication.CreateBuilder(args);
 Env.Load();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-var connectionString = Environment.GetEnvironmentVariable("POSTGRES_CONNECTION_STRING");
+
+builder.Configuration.AddEnvironmentVariables();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
